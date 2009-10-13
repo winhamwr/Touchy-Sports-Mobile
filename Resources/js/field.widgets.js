d680582dae664286256d1552edf35de492b66219
winhamwr <winhamwr@gmail.com>
2009-10-13 00:11:51 -0400

    Added player buttons with super-duper-stylish drop shadows.

--- Resources/js/field.widgets.js
+++ Resources/js/field.widgets.js
@@ -1,3 +1,3 @@
 $(document).ready(function (){
-    $('#player-button-1').append('<input type="button" value="Fred Sanford"></input>');
+    $('#player-bar > div > div').append('<button>Fred Sanford</button>');
 });
