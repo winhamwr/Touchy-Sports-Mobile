65abd24aa2e19fcc91257e8fcae503291575ae46
Andrew <n-an-a@Andrews-MacBook.local>
2009-10-19 20:16:38 -0400

    Added some android build files

---
+++ build/android/src/com/gts/gts/Gts2Activity.java
@@ -0,0 +1,7 @@
+package com.gts.gts;
+
+import org.appcelerator.titanium.TitaniumActivityGroup;
+
+public class Gts2Activity extends TitaniumActivityGroup
+{
+}
---
+++ build/android/src/com/gts/gts/Gts2Application.java
@@ -0,0 +1,7 @@
+package com.gts.gts;
+
+import org.appcelerator.titanium.TitaniumApplication;
+
+public class Gts2Application extends TitaniumApplication {
+
+}
