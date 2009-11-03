bb2c6954f85c69c60258f6475fe3242e54b53b17
areyling <areyling@gmail.com>
2009-11-02 21:14:05 -0500

    Moved button and player bar creation to uiController and removed field.widgets

--- Resources/js/field.widgets.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function (){
-    $('#player-bar > div > div').append('<button>Fred Sanford</button>');
-
-    $('#undo_b').append('<button>Undo</button>');
-    $('#turnover_b').append('<button>Turnover</button>');
-	$('#sub_b').append('<button onClick="showSub()">Sub</button>');
-});
--- Resources/js/uiController.js
+++ Resources/js/uiController.js
@@ -60,5 +60,13 @@ WebUiController.ui.prototype.displayScore = function(team, ez, score) {
 	}
 };
 
+WebUiController.ui.prototype.init = function() {
+    $('#player-bar > div > div').append('<button>Fred Sanford</button>');
+
+    $('#undo_b').append('<button>Undo</button>');
+    $('#turnover_b').append('<button>Turnover</button>');
+	$('#sub_b').append('<button onClick="showSub()">Sub</button>');
+};
+
 
 ui_controller = WebUiController.ui;
