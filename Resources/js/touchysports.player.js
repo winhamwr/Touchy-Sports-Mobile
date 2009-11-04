8337039011ba598eb60da760ab7b6414284cc040
areyling <areyling@gmail.com>
2009-11-04 16:52:39 -0500

    Modified substitution dialog and moved it to a separate js file. Updated the player and team classes.

--- Resources/js/touchysports.player.js
+++ Resources/js/touchysports.player.js
@@ -1,8 +1,12 @@
-/*
-	Player constructor
-*/
-function Player(id, name, number) {
+var ULTIMATE_STATUS_NOTSET = -1;
+var ULTIMATE_STATUS_INPLAY = 0;
+var ULTIMATE_STATUS_BENCHED = 1;
+var ULTIMATE_STATUS_INJURED = 2;
+
+ultimatePlayer = function(id, name, number, status, nickname) {
 	this.id = id;
 	this.name = name;
 	this.number = number;
+	this.status = status;
+	this.nickname = nickname;
 };
--- Resources/js/touchysports.team.js
+++ Resources/js/touchysports.team.js
@@ -1,31 +1,34 @@
 var MAX_INPLAY = 7;	// 7 players allowed on the field for Ultimate Frisbee
 
-/*
-	Team constructor
-*/
-function Team(name, coach, players) {
-	this.name = name;					// team name
-	this.coach = coach;					// coach's name
-	this.players = players;				// an array of type Player; all players on the team
-	this.playersInPlay = new Array();	// an array of type Player; all players in play
-	this.playersBenched = new Array();	// an array of type Player; all players warming the bench
-	this.playersInjured = new Array();	// an array of type Player; all broken players
-	this.sub = sub;						// the method for player substitution
-	this.requiresUpdate = false;		// if the main UI requires updating (i.e., player subbed)
-	this.test2 = test2;
-	
+if(typeof(homeTeam) == 'undefined'){
+	var homeTeam = null;
+}
+
+ultimateTeam = function(name, coach, ultimatePlayersArray) {
+	this.init(name, coach, ultimatePlayersArray);
+};
+
+ultimateTeam.prototype.init = function(name, coach, ultimatePlayersArray) {
+	this.name = name;								// team name
+	this.coach = coach;								// coach's name
+	this.playersInPlay = new Array();				// an array of type Player; all players in play
+	this.playersBenched = new Array();				// an array of type Player; all players warming the bench
+	this.playersInjured = new Array();				// an array of type Player; all broken players
+
 	// Set the first 7 players in play by default. This should be a stored lineup
 	// or set by the user before the game.  Should also set injured players, if
 	// there is any.
-	var player;
-	var numPlayers = 0;
-	for (player in players) {
-		if (numPlayers < MAX_INPLAY) {
-			this.playersInPlay[numPlayers] = this.players[player];
-		} else {
-			this.playersBenched[numPlayers-MAX_INPLAY] = this.players[player];
+	if (ultimatePlayersArray != null) {
+		var player;
+		var numPlayers = 0;
+		for (player in ultimatePlayersArray) {
+			if (numPlayers < MAX_INPLAY) {
+				this.playersInPlay[numPlayers] = ultimatePlayersArray[player];
+			} else {
+				this.playersBenched[numPlayers-MAX_INPLAY] = ultimatePlayersArray[player];
+			}
+			numPlayers++;
 		}
-		numPlayers++;
 	}
 	
 	if (this.playersInPlay < MAX_INPLAY) {
@@ -33,35 +36,18 @@ function Team(name, coach, players) {
 	}
 };
 
-function sub(leavingPlayer,enteringPlayer) {
-	var leavingIndex = findPlayerIndex(this.playersInPlay,leavingPlayer);
-	var enteringIndex = findPlayerIndex(this.playersBenched,enteringPlayer);
+ultimateTeam.prototype.sub = function(leavingPlayerName,enteringPlayerName) {
+	var leavingIndex = findPlayerByName(this.playersInPlay,leavingPlayerName);
+	var enteringIndex = findPlayerByName(this.playersBenched,enteringPlayerName);
 	
 	if (leavingIndex != -1) {
 		if (enteringIndex != -1) {
-			// get the player subbing in
-			var playerToGame = this.playersBenched.splice(enteringIndex,1);
-			// get the player subbing out, replacing the player with the sub
-			var playerToBench = this.playersInPlay.splice(leavingIndex,1,playerToGame);
-			// add player subbed out to the bench
-			this.playersBenched[this.playersBenched.length] = playerToBench;
-			console.log(playerToBench);
-			console.log(this.playersBenched);
-			
-			/*
-				The above code inserts the sub where the in game player was removed (the sub
-				will take the player's place in the playerBar.  The following code will add
-				the sub to the end of the playerBar (easier to track who has been in the game
-				longer from a glance?).
-			//
-			// remove the player subbing out from the game, saving the player object
-			var playerToBench = this.playersInPlay.splice(leavingIndex,1);
-			// remove the player subbing in from the bench, saving the player object
-			var playerToGame = this.playersBenched(enteringIndex,1);
-			// add the benched player to the bench and the player subbed in to playersInPlay
-			this.playersInPlay.push(playerToGame);
-			this.playersBenched.push(playerToBench);
-			*/
+			// store the player leaving the game
+			var leaving = this.playersInPlay[leavingIndex];
+			// replace the player leaving the game with the sub
+			this.playersInPlay[leavingIndex] = this.playersBenched[enteringIndex]
+			// remove the sub from the benchedPlayers array and add the player that left the game
+			this.playersBenched.splice(enteringIndex, 1, leaving);
 		} else {
 			console.log('Team.sub() called, player entering the game not found.');	// TODO:
 		}
@@ -71,7 +57,7 @@ function sub(leavingPlayer,enteringPlayer) {
 };
 
 // Returns the index of the player, or -1 if not found (indexOf didn't work for my player object)
-function findPlayerIndex(playerArray,playerName) {
+function findPlayerByName(playerArray,playerName) {
 	for (var i=0;i<playerArray.length;i++) {
 		if (playerArray[i].name == playerName) {
 			return i;
@@ -81,6 +67,21 @@ function findPlayerIndex(playerArray,playerName) {
 	return -1;
 };
 
-function test2() {
-	console.log(this.playersBenched);
+function loadHomeTeam() {
+	/* TODO: Load the home team from localStorage and remove the test data function */
+	createTestTeam();
 };
+
+function createTestTeam() {
+	var playerNames = new Array('Kail','Wes','Andy','Eric','Jack','Jim','Jose',
+		'Alpha','Beta','Chi','Delta','Epsilon','Gamma','Omega','Sigma','Theta','Zeta');
+	var teamPlayers = new Array();
+	for (i=0; i<playerNames.length; i++) {
+		teamPlayers[i] = new ultimatePlayer(i,playerNames[i],i,ULTIMATE_STATUS_NOTSET,playerNames[i]);
+	}
+	homeTeam = new ultimateTeam('Stepdads','Mini Me',teamPlayers);
+};
+
+$(document).ready(function() {
+	loadHomeTeam();
+});
