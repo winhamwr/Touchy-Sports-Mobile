eb5978b7b5fb09574b338955dc47d1ffa76f9bb9
areyling <areyling@gmail.com>
2010-01-04 23:47:59 -0500

    Fixed team management and added the ability to save and load the team.

--- Resources/js/editPlayers.js
+++ Resources/js/editPlayers.js
@@ -2,19 +2,86 @@ $(document).ready(function(){
     $("#buttonAdd").click(function() {
         addPlayer();
     });
+	$('#clearTeam').click(function() {
+		clearTeam();
+	});
+	$('#buttonSave').click(function() {
+		saveTeam();
+	});
+	loadTeam();
 });
 
 function addPlayer() {
-    console.log("addPlayer");
     var ni = document.getElementById('content');
     var numi = document.getElementById('theValue');
     var num = (document.getElementById("theValue").value -1)+ 2;
     numi.value = num;
-    var divIdName = "my"+num+"Div";
-    $("#content").append("<div id='"+divIdName+"'><input type=\"text\" value=\"Name\"</input> <a href=\"javascript:;\" onclick=\"removePlayer(\'"+divIdName+"\')\">Remove</a></div>");
+    var divIdName = "p"+num;
+    $("#content").append("<div id='"+divIdName+"'><input type=\"text\" value=\"Player\"</input> <a href=\"javascript:;\" onclick=\"removePlayer(\'"+divIdName+"\')\">Remove</a></div>");
 };
 
 function removePlayer(player) {
-    console.log("removePlayer");
     $("#"+player).remove();
+};
+
+// Until we have the team creation/loading working, just going to
+// generate a team and load them here for testing.
+function createTestTeam() {
+	var player_names = new Array('Kail','Wes','Andy','Eric','Jack','Jim','Jose',
+		'Alpha','Beta','Chi','Delta','Epsilon','Gamma','Omega','Sigma','Theta','Zeta');
+
+	var players = new Array();
+	$.each(player_names, function(i, name){
+		var player = new UltimatePlayer(name);
+		players.push(player);
+	});
+
+	var home_team = new UltimateTeam('Stepdads', players);
+
+	var starting_players = players.slice(0, 7);
+	home_team.setStartingLineup(starting_players);
+
+	return home_team;
+};
+
+function loadTeam() {
+	var currentTeam;
+	var db = new db_controller();
+	if (db.gameExists()) {
+		function foo(){};
+		var tempGame = new foo();
+		db.loadGame(tempGame);
+		currentTeam = tempGame['home_team'];
+	} else {
+		currentTeam = createTestTeam();
+	}
+	
+	var playerCount = -1;
+	$.each(currentTeam.players, function() {
+			playerCount++;
+			addPlayer();
+			var $lastDiv = $('#content > div:last input');
+			var playerName = currentTeam.players[playerCount].name;
+			$lastDiv.val(playerName);
+		});
+};
+
+function saveTeam() {
+	var players = new Array();
+	var $playerDivs = $('#content > div');
+	$.each($playerDivs, function() {
+		var playerName = ($(this).children().val());
+		players.push(playerName);
+	});
+	var newTeam = new UltimateTeam('StepDads', players);
+	newTeam.setStartingLineup(newTeam.players.slice(0,UltimateTeam.MAX_IN_PLAY));
+	localStorage.setItem("CURRENT_TEAM", JSON.stringify(newTeam));
+};
+
+function clearTeam() {
+	var $divs = $('#content > div');
+	$.each($divs, function() {
+		$(this).remove();
+	});
+	addPlayer();
 };
