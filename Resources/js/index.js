d30df3b79571498cf61a8fab1a12b5785d71818f
winhamwr <winhamwr@gmail.com>
2010-01-05 00:17:01 -0500

    Added some minimal navigation between the data management, player management and field pages.

--- Resources/js/index.js
+++ Resources/js/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	$('div').append('<h1>TouchySports Mobile</h1></br>'+
-					'<a href="field.html">Test Field View</a></br>'+
+					'<a href="editPlayers.html">Select Team</a></br>'+
+					'<a href="data.html">Manage Data</a></br>'+
 					'<a href="about.html">About</a>');
 });
