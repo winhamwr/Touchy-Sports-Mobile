9a2abf1e4d11b307c50739559c00cc97830d3f2a
winhamwr <winhamwr@gmail.com>
2010-01-30 18:11:57 -0500

    Converted the editPlayers ui controller to be a jquery plugin

--- Resources/js/editPlayers.js
+++ Resources/js/editPlayers.js
@@ -1,5 +1,38 @@
+;(function($) {
 
-function addPlayer() {
+/**
+ * Controls the UI for editing/creating an UltimateTeam.
+ *
+ * @param editor_div The div where the editor fields and controls will live.
+ * @param options An options object for configuring the editor.
+ **/
+TeamEditor = function(editor_div, options) {
+	this.editor_dev = editor_div;
+	this._options = options;
+}
+
+TeamEditor.prototype.init = function() {
+	this.bindListeners();
+}
+
+/**
+ * Bind all of the UI control listeners for handling actions.
+ **/
+TeamEditor.prototype.bindListeners = function() {
+	
+	this._options.add_player_button.click(function() {
+		this.addPlayer();
+	});
+	this._options.clear_team_button.click(function() {
+		this.clearTeam();
+	});
+	this._options.save_team_button.click(function() {
+		this.saveTeam();
+	});
+
+}
+
+TeamEditor.prototype.addPlayer = function() {
     var ni = document.getElementById('content');
     var numi = document.getElementById('theValue');
     var num = (document.getElementById("theValue").value -1)+ 2;
@@ -8,13 +41,13 @@ function addPlayer() {
     $("#content").append("<div id='"+divIdName+"'><input class='webInput' type=\"text\" value=\"Player\"</input> <a class='webButton removeLink' href=\"javascript:;\" onclick=\"removePlayer(\'"+divIdName+"\')\">Remove</a></br></br></div>");
 };
 
-function removePlayer(player) {
+TeamEditor.prototype.removePlayer = function(player) {
     $("#"+player).remove();
 };
 
 // Until we have the team creation/loading working, just going to
 // generate a team and load them here for testing.
-function createTestTeam() {
+TeamEditor.prototype.createTestTeam = function() {
 	var player_names = new Array('Kail','Wes','Andy','Eric','Jack','Jim','Jose',
 		'Alpha','Beta','Chi','Delta','Epsilon','Gamma','Omega','Sigma','Theta','Zeta');
 
@@ -32,29 +65,29 @@ function createTestTeam() {
 	return home_team;
 };
 
-function loadTeam() {
-	var currentTeam;
-	var db = new db_controller();
-	if (db.gameExists()) {
-		function foo(){};
-		var tempGame = new foo();
-		db.loadGame(tempGame);
-		currentTeam = tempGame['home_team'];
-	} else {
-		currentTeam = createTestTeam();
-	}
-	
-	var playerCount = -1;
-	$.each(currentTeam.players, function() {
-			playerCount++;
-			addPlayer();
-			var $lastDiv = $('#content > div:last input');
-			var playerName = currentTeam.players[playerCount].name;
-			$lastDiv.val(playerName);
-		});
-};
+//function loadTeam() {
+//	var currentTeam;
+//	var db = new db_controller();
+//	if (db.gameExists()) {
+//		function foo(){};
+//		var tempGame = new foo();
+//		db.loadGame(tempGame);
+//		currentTeam = tempGame['home_team'];
+//	} else {
+//		currentTeam = createTestTeam();
+//	}
+//
+//	var playerCount = -1;
+//	$.each(currentTeam.players, function() {
+//			playerCount++;
+//			addPlayer();
+//			var $lastDiv = $('#content > div:last input');
+//			var playerName = currentTeam.players[playerCount].name;
+//			$lastDiv.val(playerName);
+//		});
+//};
 
-function saveTeam() {
+TeamEditor.prototype.saveTeam = function() {
 	var players = new Array();
 	var $playerDivs = $('#content > div');
 	$.each($playerDivs, function() {
@@ -65,7 +98,7 @@ function saveTeam() {
 	sessionStorage.setItem("CURRENT_TEAM", JSON.stringify(newTeam));
 };
 
-function clearTeam() {
+TeamEditor.prototype.clearTeam = function() {
 	var $divs = $('#content > div');
 	$.each($divs, function() {
 		$(this).remove();
@@ -73,26 +106,21 @@ function clearTeam() {
 	addPlayer();
 };
 
-function resizeUi() {
-	useMobileClasses();
-};
+/*
+ * Registers the jquery plugin function
+ */
+$.fn.teamEditor = function(options) {
 
-function useMobileClasses() {
-	var webClasses = ['webLink', 'webTitle', 'webButton', 'webInput'];
-	var mobileClasses = ['mobileLink', 'mobileTitle', 'mobileButton', 'mobileInput'];
-	
-	$.each(webClasses, function(i, myClass) {
-		$('.' + myClass).addClass(mobileClasses[i]);
-		$('.' + myClass).removeClass(myClass)
-	});
-};
+	defaults = {
+		'add_player_button':		$('#buttonAdd'),
+		'clear_team_button':		$('#clearTeam'),
+		'save_team_button':			$('#buttonSave')
+	};
 
-function useWebClasses() {
-	var webClasses = ['webLink', 'webTitle', 'webButton', 'webInput'];
-	var mobileClasses = ['mobileLink', 'mobileTitle', 'mobileButton', 'mobileInput'];
-	
-	$.each(mobileClasses, function(i, myClass) {
-		$('.' + myClass).addClass(webClasses[i]);
-		$('.' + myClass).removeClass(myClass)
-	});
+	options = $.extend(defaults, options);
+
+	var te = new TeamEditor($(this), options);
+	te.init();
+	return te;
 };
+})(jQuery);
