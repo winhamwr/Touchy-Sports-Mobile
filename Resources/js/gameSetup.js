debd96f258596e4009d4d9cefc6aff2ab4389446
Kail <kfkkail@kfkkail-laptop.(none)>
2010-01-30 13:07:22 -0500

    added all session storage for gameSetup

--- Resources/js/gameSetup.js
+++ Resources/js/gameSetup.js
@@ -6,12 +6,23 @@ $(document).ready(function(){
 });
 
 function saveGameSetup() {
+    // save opposing team name
+    var opposingTeamName = $('#opposingTeamName').val();
+    console.log(opposingTeamName);
+    sessionStorage.setItem('opposing_team_name', opposingTeamName);
+
+    // save disc possession
+    var possession = $('#possession').val();
+    console.log(possession);
+    sessionStorage.setItem('team_possession', possession);
+
+    // save field direction
+    var direction = $('#direction').val();
+    console.log(direction);
+    sessionStorage.setItem('my_team_direction', direction);
+
     // save total players in play
-    var totalPlayersInPlay = parseInt($('#totalPlayers').val());
+    var totalPlayersInPlay = $('#totalPlayers').val();
     console.log(totalPlayersInPlay);
     sessionStorage.setItem('max_players_in_play', totalPlayersInPlay);
-    //$('#opposingTeamName').val()
-    //$('#possession').val()
-    //$('#direction').val()
-    //$('#totalPlayers').val()
 }
