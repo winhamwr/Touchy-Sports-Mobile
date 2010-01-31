49fb7fa8abdcb951ffc79b713cb7ad6fb30905e1
winhamwr <winhamwr@gmail.com>
2010-01-30 20:40:48 -0500

    Renamed the editPlayers page to create_team.html and renamed the corresponding javascript and css files.

--- Resources/js/index.js
+++ Resources/js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$('div').append('<h1>TouchySports Mobile</h1></br></br>'+
-					'<p><a href="editPlayers.html">Select Team</a></p>'+
+					'<p><a href="create_team.html">Create Team</a></p>'+
 					'<p><a href="data.html">Manage Data</a></p>'+
 					'<p><a href="about.html">About</a></p>');
 });
